87bef8b961ccb1bfa8871c39d78535ae3eb66677
kmaasrud <km@aasrud.com>
2021-02-04 17:49:03 +0100

    I think I solved Metropolis now... Doing things a bit different

--- vmc/src/main.rs
+++ vmc/src/main.rs
@@ -5,11 +5,10 @@ mod system;
 pub use particle::Particle;
 pub use system::System;
 pub use system::HarmonicTrap;
-pub use solver::SimpleMetropolis;
+pub use solver::BruteForceMetropolis;
 
 
 fn main() {
     let boson = Particle::new(&[3., 2., 1.]);
     println!("{:?}", boson);
-    SimpleMetropolis::new().solve(-10., 10)
 }
--- vmc/src/solver.rs
+++ vmc/src/solver.rs
@@ -5,20 +5,19 @@ use rand::distributions::{Uniform, Distribution};
 trait Metropolis {
     // Samples a new step based on `curr_step`. Does not update
     // the value of `self.curr_step`.
-    fn sample(&mut self, curr_step: &Vec<f64>) -> Vec<f64> {
+    fn do_change(&mut self) -> bool {
         let mut rng = thread_rng();
-        let next_step: Vec<f64> = self.next_step();
         let uniform = Uniform::new(0., 1.);
         
-        if uniform.sample(&mut rng) < self.acceptance_factor(next_step) {
-            next_step
+        if uniform.sample(&mut rng) < self.acceptance_factor() {
+            true
         } else {
-            *curr_step
+            false
         }
     }
     
-    fn acceptance_factor(&self, next_step: Vec<f64>) -> f64;
-    fn next_step(&self) -> Vec<f64>;
+    fn acceptance_factor(&self) -> f64;
+    fn next_step(&mut self);
 }
 
 // Struct for representing a brute force Metropolis algorithm.
@@ -33,10 +32,11 @@ trait Metropolis {
 // bf_metro.step();
 // println!("{}", bf_metro.curr_step);
 // ```
-struct BruteForceMetropolis {
+pub struct BruteForceMetropolis {
     // `curr_step` describes the N particles with D dimensions as a N * D dimensional vector.
     // This is computationally preferrable and allows for more general code. Do keep this in mind.
     curr_step: Vec<f64>,
+    next_step: Vec<f64>,
     step_size: f64,
 }
 
@@ -44,24 +44,30 @@ impl BruteForceMetropolis {
     // Makes a new `BruteForceMetropolis` struct based on a step size.
     fn new(step_size: f64) -> Self {
         // Initialize with random `curr_step`. Just setting empty for now, so the vector needs to be filled.
-        Self{ curr_step: vec![], step_size: step_size, }
+        Self{ curr_step: vec![], next_step: vec![], step_size: step_size, }
     }
 
     // Makes a new step based on `curr_step`. Also updates
     // the value of `self.curr_step`.
-    fn step(&mut self) -> Vec<f64> {
-        self.curr_step = self.sample(&self.curr_step);
-        self.curr_step
+    fn step(&mut self) -> &Vec<f64> {
+        self.next_step();
+        if self.do_change() {
+            &self.curr_step
+        } else {
+            &self.next_step
+        }
     }
 }
 
 impl Metropolis for BruteForceMetropolis {
     // Calculates the acceptance factor based on the current step (stored in the struct) and the next step. 
     // TODO: This could perhaps actually be implemented as part of the trait.
-    fn acceptance_factor(&self, next_step: Vec<f64>) -> f64 {
-        // TODO: We need WaveFunction structs
-        let wave_function_old: f64 = 1.;
-        let wave_function_new: f64 = 1.;
+    fn acceptance_factor(&self) -> f64 {
+        // TODO: We need WaveFunction structs, the below is just random rubbish now
+        let wave_function_old: f64 = self.curr_step.iter().sum();
+        let wave_function_new: f64 = self.next_step.iter().sum();
+        
+        // Not random rubbish anymore
         let hastings_ratio: f64 = wave_function_new.powi(2) / wave_function_old.powi(2);
 
         // Return hastings ratio if it is smaller than 1, else 1
@@ -70,16 +76,16 @@ impl Metropolis for BruteForceMetropolis {
     
     // This is what makes this a brute force method, as `BruteForceMetropolis` only makes
     // a random step in either direction. 
-    fn next_step(&self) -> Vec<f64> {
+    fn next_step(&mut self) {
         // thread_rng() randomly chooses either `1.` or `-1.`. Don't know if this is the most efficient way, but it should work...
         let uniform = Uniform::new(0., 1.);
-        let next_step: Vec<f64> = vec![];
+        let mut next_step: Vec<f64> = vec![];
         for i in 0..self.curr_step.len() {
             // This is what Morten does in [this example](https://compphysics.github.io/ComputationalPhysics2/doc/pub/week2/html/week2.html#___sec11)
             // I trust him being correct, but I am not totally sure about the `- 0.5` part...
             next_step.push(self.curr_step[i]
                 + (uniform.sample(&mut thread_rng()) - 0.5) * self.step_size);
         }
-        next_step
+        self.next_step = next_step;
     }
 }
