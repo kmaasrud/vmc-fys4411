56545ec53f604e34ac0d498e291bc02e9492efd6
kmaasrud <km@aasrud.com>
2021-02-11 16:46:28 +0100

    Added drift_force function to WaveFunction trait, based on @Schoyen's work

--- vmc/src/wavefunction.rs
+++ vmc/src/wavefunction.rs
@@ -1,6 +1,10 @@
 use crate::Particle;
 
 pub trait WaveFunction {
-    fn new() -> Self;
     fn laplace(&self, particles: &Vec<Particle>) -> f64;
+    fn gradient(&self, i_particle: usize) -> Vec<f64>;
+    // Here I'm just going by what @Schoyen has done, let's see if it works
+    fn drift_force(&self, i_particle: usize) -> Vec<f64> {
+        self.gradient(i_particle).iter().map(|x| x * 2.).collect()
+    }
 }
