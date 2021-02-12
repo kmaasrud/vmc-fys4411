245fc2a03604ccd21df410406dbe93e68b4f7343
kmaasrud <km@aasrud.com>
2021-02-12 17:44:00 +0100

    Removed erroneous docstrings

--- vmc/src/solver.rs
+++ vmc/src/solver.rs
@@ -4,8 +4,6 @@ use crate::{System, WaveFunction, Particle};
 
 /// Trait for Metropolis samplers.
 trait Metropolis {
-    /// Evaluates whether or not a step should be takeng, based on the
-    /// current curr_step and next_step. Returns a Boolean.
     fn do_change<T: WaveFunction>(&mut self, sys: &System<T>) -> bool {
         let mut rng = thread_rng();
         let uniform = Uniform::new(0., 1.);
@@ -31,20 +29,17 @@ pub struct BruteForceMetropolis {
 impl BruteForceMetropolis {
     /// Makes a new `BruteForceMetropolis` struct based on a step size.
     fn new(step_size: f64) -> Self {
-        // Initialize with random `curr_step`. Just setting empty for now, so the vector needs to be filled.
         Self{ next_step: vec![], step_size: step_size, }
     }
 }
 
 impl Metropolis for BruteForceMetropolis {
-    /// Makes a new step based on `curr_step`. Also updates the value of `self.curr_step`.
     fn step<T: WaveFunction>(&mut self, sys: &mut System<T>) {
         if !self.do_change(sys) {
             sys.particles = self.next_step.clone();
         }
     }
 
-    /// Calculates the acceptance factor based on the current step (stored in the struct) and the next step.
     fn acceptance_factor<T: WaveFunction>(&mut self, sys: &System<T>) -> f64 {
         let wf_old: f64 = sys.wavefunction.evaluate(&sys.particles);
         self.next_step = sys.random_particle_change(self.step_size);
