d48416a66df29cf12a6cdf70e3bddcf341c156fa
kmaasrud <km@aasrud.com>
2021-02-12 17:38:49 +0100

    Fixed typo

--- vmc/src/hamiltonian.rs
+++ vmc/src/hamiltonian.rs
@@ -2,6 +2,6 @@ use crate::System;
 
 trait Hamiltonian {
     pub fn kinetic_energy(system: System) -> f64 {
-        - system.omega / 2. * system.wavefunction.laplacian(system.particles)
+        - system.omega / 2. * system.wavefunction.laplace(system.particles)
     }
 }
