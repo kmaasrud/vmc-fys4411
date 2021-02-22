9dbd0071f4582861abb438700f19720a1e46a4f0
kmaasrud <km@aasrud.com>
2021-02-22 22:16:59 +0100

    Monte Carlo integration basically done. Needs refinement

--- vmc/src/montecarlo.rs
+++ vmc/src/montecarlo.rs
@@ -2,15 +2,29 @@ use crate::System;
 use crate::WaveFunction;
 use crate::{Metropolis, MetropolisResult};
 
+/// Does Monte Carlo integration over the WaveFunction of a System, using a given Metropolis
+/// algorithm.
+///
+/// **Parameters**:
+/// - n: usize -- The number of Monte Carlo cycles to perform
+/// - sys: &mut System<V: WaveFunction> -- Reference to a System struct containing a WaveFunction
+/// - metro: &mut T where T: Metropolis -- Reference to a Metropolis struct
 fn monte_carlo<T: Metropolis, V: WaveFunction>(n: usize, sys: &mut System<V>, metro: &mut T) -> f64 {
     let mut result: f64 = 0.;
+    let mut prev: f64 = sys.wavefunction.evaluate(&sys.particles);
+    // Not sure how we should define the interval, and thus not sure how to calculate v, but this
+    // should be pretty straight forward.
+    let v: f64 = 1.;
 
-    // Just some gibberish for now, but the general structure holds
     for _ in 0..n {
         match metro.step(sys) {
-            MetropolisResult::Accepted(val) => result += val,
-            MetropolisResult::Rejected => result += 1.,
+            MetropolisResult::Accepted(val) => {
+                result += val;
+                prev = val;
+            },
+            MetropolisResult::Rejected => result += prev,
         }
     }
-    result
+
+    v * result / (n as f64)
 }
