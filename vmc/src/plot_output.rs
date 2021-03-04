b63f464511cf7e97e8cde71625d6a7c899d9ff0c
annasro <anna.stray.rongve@gmail.com>
2021-03-04 16:47:50 +0100

    general x,y plotter

--- vmc/src/plot_output.rs
+++ vmc/src/plot_output.rs
@@ -3,7 +3,7 @@ use std::io::prelude::*;
 
 
 fn main() -> std::io::Result <(), Error>{
-    let mut file = File::create("file.txt")?; //read ionly mode
+    let mut file = File::create("file.txt")?; //read only mode
     let mut buf_reader = BufReader::new(file);
     let mut contents = String::new();
     buf_reader.read_to_string(&mut contents)?;
