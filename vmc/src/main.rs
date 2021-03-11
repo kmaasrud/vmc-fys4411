5c6bebede260e028f47372e3c77bf803d6064a99
kmaasrud <km@aasrud.com>
2021-03-11 10:07:28 +0100

    Decoupled some variables in main for easier testing

--- vmc/src/main.rs
+++ vmc/src/main.rs
@@ -13,9 +13,15 @@ use montecarlo::monte_carlo;
 
 
 fn main() {
-    let wf: GaussianWaveFunction = GaussianWaveFunction::new(0.5);
+    let alpha = 0.5;
+    let mc_cycles = 1_000_000;
+    let n_particles = 10;
+    let dimensions = 3;
+    let step_size = 0.1;
+
+    let wf: GaussianWaveFunction = GaussianWaveFunction::new(alpha);
     let ham: HarmonicOscillator = HarmonicOscillator::elliptical(0.5, 1.);
-    let mut test_system: System<GaussianWaveFunction, HarmonicOscillator> = System::distributed(1, 3, wf, ham, 2.);
-    let mut metro: BruteForceMetropolis = BruteForceMetropolis::new(0.01);
-    println!("{}", monte_carlo(10000, &mut test_system, &mut metro));
+    let mut test_system: System<GaussianWaveFunction, HarmonicOscillator> = System::distributed(n_particles, dimensions, wf, ham, 0.1);
+    let mut metro: BruteForceMetropolis = BruteForceMetropolis::new(step_size);
+    println!("{}", monte_carlo(mc_cycles, &mut test_system, &mut metro));
 }
