115a7bdd218a9c906ab61bf165f152b11d79a4b3
kmaasrud <km@aasrud.com>
2021-03-20 15:30:59 +0100

    Lots of tidywork in main

--- vmc/src/main.rs
+++ vmc/src/main.rs
@@ -15,105 +15,73 @@ pub use hamiltonian::Hamiltonian;
 use montecarlo::monte_carlo;
 use threadpool::ThreadPool;
 
-use std::time:: Instant;
+use std::{
+    time::Instant,
+    fs::{File, create_dir_all},
+    path::Path,
+    io::prelude::*
+};
+use num_cpus;
 
-extern crate num_cpus;
 
-use std::fs::OpenOptions;
-use std::io::{BufWriter, Write};
+const STEP_SIZE: f64 = 1.0;
+const ALPHAS: [f64; 8] = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9];
+const N_LIST: [usize; 3] = [1, 10, 100];
+const MC_CYCLES: usize = 10_000;
+const CSV_HEADER: &str = "Alpha,Energy,Energy2,TimeElapsed\n";
 
 
 fn main() {
-    //let alpha = 0.5;
-    //let n_particles = 1 ;
-    //let dimensions = 1;
-    let step_size = 1.0;
-    let total_mc_cycles = 10_000;
-    
+    let n_cpus = num_cpus::get();
+    println!("Found {} cores!", n_cpus);
 
+    let mc: usize = MC_CYCLES / n_cpus;
+    println!("Running {} Monte Carlo cycles on each core.", mc);
 
-    let alpha_list: Vec<f64> = vec![0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9];
-    let particle_list = vec![1,10,100];
+    let pool = ThreadPool::new(n_cpus as u8);
 
-    let variance = 1;
-    let accept_ratio = 1;
-
-    let header = "Alpha,Energy,Energy2,Variance,AcceptRatio,ElapesdTime\n";
-    
     let start = Instant::now();
-
-    let cpus = num_cpus::get();
-    let mc_cycles:usize = total_mc_cycles / cpus;
-    println!("{}", mc_cycles);
-    let pool = ThreadPool::new(cpus as u8);
-    for _ in 0..cpus {
-        let alc = alpha_list.clone();
-        let plc = particle_list.clone();
-        pool.execute(move ||run_sim(alc.clone(), plc.clone(), variance, accept_ratio, header, start, step_size, mc_cycles));
+    for _ in 0..n_cpus {
+        pool.execute(move ||run_sim(start, mc));
     }
 
-    println!("All {} cores now executing, waiting for them to finish", cpus);
+    println!("All cores now executing, waiting for them to finish...");
     pool.join_all();
 
     println!("Total time spent: {:?}", start.elapsed());
-
-
 }
 
 
+fn run_sim(start: Instant, mc_cycles: usize) {
+    let path = format!("./data/numeric/{:?}/", std::thread::current().id());
+    if Path::new(&path).exists() == false {
+        create_dir_all(&path).expect("Unable to create folder");
+    }
 
-fn run_sim(alpha_list: Vec<f64>, particle_list:Vec<usize>, variance:i32, accept_ratio:i32, header: &str, start:Instant, step_size:f64, mc_cycles:usize) {
     for dim in 1..=3{
-        //println!("dim: {}",dim);
+        for n in N_LIST.iter(){
+            println!("Calculating: dim: {}, n_part {}, {:?}", dim, n, std::thread::current().id());
 
-        for n_particles in particle_list.iter(){
-            println!("Calculating: dim: {}, n_part {}, {:?}",dim, n_particles, std::thread::current().id());
-            //dummypath
-            //let path = format!("./data/dummydata/dummy_{}D_{}_particles.csv", dim, n_particles);
-            //path for analytical results
-            //let path = format!("./data/analytic/experiment_{}D_{}_particles_ana.csv", dim, n_particles);
-            //path numerical results
-            
-            let path = format!("./data/numeric/{:?}/", std::thread::current().id());
-            if std::path::Path::new(&path).exists() == false {
-                std::fs::create_dir_all(path).expect("Can't create folder");
-            }
-            let filepath = format!("./data/numeric/{:?}/experiment_{}D_{}_n_part.csv", std::thread::current().id(), dim, n_particles );
-            //let path = format!("./data/non_paralell/numeric/experiment_{}D_{}_particles_num_{:?}.csv", dim, n_particles, std::thread::current().id());
-            //let path_ana = format!("./data/non_paralell/analytic/experiment_{}D_{}_particles_num_{:?}.csv", dim, n_particles, std::thread::current().id());
-            let file = OpenOptions::new()
-                        .read(true)
-                        .append(true)
-                        .create(true)
-                        .open(filepath)
-                        .expect("Unable to open file");
-            //let file_ana = OpenOptions::new().read(true).append(true).create(true).open(path_ana).expect("Unable to open file");
-            let mut f = BufWriter::new(file);
-            //let mut f_ana = BufWriter::new(file_ana);
-            
-            f.write_all(header.as_bytes()).expect("Unable to write data"); 
-            //f_ana.write_all(header.as_bytes()).expect("Unable to write data"); 
+            let filepath = &format!("{}/experiment_{}D_{}_n_part.csv", &path, dim, n);
+            let mut f = match File::create(&Path::new(filepath)) {
+                Ok(f) => f,
+                Err(why) => panic!("Unable to create {}: {}", filepath, why),
+            };
 
+            f.write_all(CSV_HEADER.as_bytes()).expect("Unable to write data"); 
 
-            for alpha in alpha_list.iter(){
+            for alpha in ALPHAS.iter(){
                 let wf: GaussianWaveFunction = GaussianWaveFunction::new(*alpha);
                 let ham: Hamiltonian = Hamiltonian::elliptical(1.0, 1.0);
-                let mut test_system: System<GaussianWaveFunction> = System::distributed(*n_particles, dim, wf, ham, 0.1);
-                let mut metro: BruteForceMetropolis = BruteForceMetropolis::new(step_size);
-                //println!("Energy from monte carlo calculations {}", monte_carlo(mc_cycles, &mut test_system, &mut metro)); 
+                let mut test_system: System<GaussianWaveFunction> = System::distributed(*n, dim, wf, ham, 0.1);
+                let mut metro: BruteForceMetropolis = BruteForceMetropolis::new(STEP_SIZE);
                 let energy = monte_carlo(mc_cycles, &mut test_system, &mut metro); 
                 let energy2 = energy.powi(2);
 
-                //let energy_ana = local_energy_analytical(alpha, dim, &test_system.particles);
-                //let energy2_ana = energy_ana.powi(2);
-
                 let duration = start.elapsed();
-                //println!("Time used in seconds {:?} = {:?} min",duration, duration/60);
-                let data = format!("{},{},{},{},{},{:?}\n", alpha, energy, energy2, variance, accept_ratio, duration);
-                //let data_ana = format!("{},{},{},{},{},{:?}\n", alpha, energy_ana, energy2_ana, variance, accept_ratio, duration);
+                let data = format!("{},{},{},{:?}\n", alpha, energy, energy2, duration);
                 
                 f.write_all(data.as_bytes()).expect("Unable to write data");
-                //f_ana.write_all(data_ana.as_bytes()).expect("Unable to write data");
             }
         }
     }
