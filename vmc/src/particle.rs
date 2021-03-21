6ca098f6ba1417cf716509997685448f1a9aad35
kmaasrud <km@aasrud.com>
2021-03-21 12:12:07 +0100

    Even more docstringsgit add vmc/src/particle.rs !

--- vmc/src/particle.rs
+++ vmc/src/particle.rs
@@ -1,9 +1,4 @@
 /// Struct that represents a single particle.
-///
-/// # Attributes
-///
-/// - `position: Vec<f64>` - The position of the particle, with an arbitrary dimensionality.
-/// - `dimensions: u8` - The dimensionality of `position`.
 #[derive(Debug, Clone)]
 pub struct Particle {
     pub position: Vec<f64>,
@@ -14,14 +9,6 @@ pub struct Particle {
 impl Particle {
     /// Creates a new particle with a given dimensionality.
     /// The particle's initial position is set to 0.
-    ///
-    /// # Examples
-    ///
-    /// Basic usage:
-    ///
-    /// ```
-    /// let boson = Particle::new(3)
-    /// ```
     pub fn new(dim: usize) -> Particle {
         Particle{
             position: vec![0.; dim],
@@ -30,20 +17,22 @@ impl Particle {
         }
     }
 
-    /// Computes the sum of each coordinate squared
+    /// Computes the squared sum of each coordinate.
     pub fn squared_sum(&self) -> f64 {
         self.position.iter().map(|x| x.powi(2)).sum()
     }
 
-    pub fn squared_sum_scaled_z(&self, lambda: &f64) -> f64 {
+    /// Computes the squared sum of each coordinate, but the z-component is scaled by beta: &f64
+    pub fn squared_sum_scaled_z(&self, beta: &f64) -> f64 {
         match self.dim {
             1 => self.position[0].powi(2),
             2 => self.position[0].powi(2) + self.position[1].powi(2),
-            3 =>  self.position[0].powi(2) + self.position[1].powi(2) + lambda * self.position[2].powi(2),
+            3 => self.position[0].powi(2) + self.position[1].powi(2) + beta * self.position[2].powi(2),
             _ => panic!("Dimension should be 1, 2 or 3.")
         }
     }
     
+    /// Returns the distance of this particle to other: &Particle.
     pub fn distance_to(&self, other: &Particle) -> f64 {
         let result: f64 = other.position.iter()
             .zip(self.position.iter())
