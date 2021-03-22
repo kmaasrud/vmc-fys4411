0ebbce9257fe9babcb648c890c55a10aeb4487b7
kmaasrud <km@aasrud.com>
2021-03-22 12:05:44 +0100

    Parallellize over step sizes, do not need multiple runs here

--- vmc/src/produce_output.rs
+++ vmc/src/produce_output.rs
@@ -19,7 +19,7 @@ use num_cpus;
 use itertools_num::linspace;
 
 
-/// Produces results for dimentions 1-3, different alphas and different number of particles and
+/// Produces results for dimensions 1-3, different alphas and different number of particles and
 /// saves these in its own separate file. Does this a number of times corresponding to the number
 /// of cores the CPU running the program has.
 pub fn dim_and_n() {
@@ -75,62 +75,60 @@ pub fn dim_and_n() {
 }
 
 
+/// Runs the VMC for dimension 1-3, different values of alpha and different step sizes. 
+/// Does this using both brute force Metropolis sampling and importance Metropolis sampling.
 pub fn bruteforce_vs_importance() {
-    const N: usize = 10;
+    const N: usize = 100;
     const ALPHAS: [f64; 8] = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9];
     const MC_CYCLES: usize = 1_000;
     const CSV_HEADER: &str = "StepSize,Alpha,Energy,Energy2\n";
 
     fn run_for_sampler<T: Metropolis>() {
-
         println!("Running simulations using {} algorithm...", std::any::type_name::<T>().split("::").last().unwrap());
 
-        fn run_sim<T: Metropolis>(mc: usize) {
-            
+        fn run_sim<T: Metropolis>(mc: usize, step_size: f64) {
             for dim in 1..=3 {
                 println!("Dimension: {}", dim);
-                let path = format!("./data/bruteforce_vs_importance/{}/{:?}", std::any::type_name::<T>().split("::").last().unwrap(),std::thread::current().id());
+                let path = format!("./data/bruteforce_vs_importance/{}/step_size{}", std::any::type_name::<T>().split("::").last().unwrap(), step_size);
                 create_dir(&path);
 
                 let mut f = create_file(&format!("{}/{}D.csv", &path, dim));
-                f.write_all(CSV_HEADER.as_bytes()).expect("Unable to write data"); 
-                for step_size in linspace(0.1, 1., 10) {
-                    for alpha in ALPHAS.iter() {
-                        let wf = WaveFunction::new(*alpha);
-                        let ham: Hamiltonian = Hamiltonian::spherical();
-                        let mut system: System = System::distributed(N, dim, wf, ham, 0.1);
-                        let mut metro: T = T::new(step_size);
-                        let vals = monte_carlo(mc, &mut system, &mut metro); 
+                f.write_all(CSV_HEADER.as_bytes()).expect("Unable to write data");
 
-                        let data = format!("{},{},{},{}\n", step_size, alpha, vals.energy, vals.energy_squared);
-                        f.write_all(data.as_bytes()).expect("Unable to write data");
-                    }
-                } 
+                for alpha in ALPHAS.iter() {
+                    let wf = WaveFunction::new(*alpha);
+                    let ham: Hamiltonian = Hamiltonian::spherical();
+                    let mut system: System = System::distributed(N, dim, wf, ham, 0.1);
+                    let mut metro: T = T::new(step_size);
+                    let vals = monte_carlo(mc, &mut system, &mut metro); 
+
+                    let data = format!("{},{},{},{}\n", step_size, alpha, vals.energy, vals.energy_squared);
+                    f.write_all(data.as_bytes()).expect("Unable to write data");
+                }
             }
         }
 
         // Multithreading
         let n_cpus = num_cpus::get();
         let mc: usize = MC_CYCLES / n_cpus;
+
         println!("Spawning threadpool of {} threads, with {} Monte Carlo cycles on each", &n_cpus, &mc);
         let pool = ThreadPool::new(n_cpus as u8);
-
         let start = Instant::now();
 
-        for _ in 0..n_cpus {
-            pool.execute(move || run_sim::<T>(mc)); //Running the simulation on each thread individually
+        for cpu_i in 1..=n_cpus {
+            pool.execute(move || run_sim::<T>(mc, (cpu_i as f64) / (n_cpus as f64))); //Running the simulation on each thread individually
         }
         println!("All {} threads now executing, waiting for them to finish...", n_cpus);
         pool.join_all();
         
         println!("Time spent: {:?}", start.elapsed());
     }
-    
-
 
     run_for_sampler::<BruteForceMetropolis>();
     run_for_sampler::<ImportanceMetropolis>();
 }
+
 
 fn create_file(filepath: &str) -> File {
     match File::create(&Path::new(filepath)) {
