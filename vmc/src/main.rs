ecc30a70d11101c6c5488164cdf209dcf31c783b
amundmr <amund.raniseth@gmail.com>
2021-03-24 22:06:42 +0100

    no går i hjem

--- vmc/src/main.rs
+++ vmc/src/main.rs
@@ -18,6 +18,7 @@ pub use threadpool::ThreadPool;
 
 
 fn main() {
-    produce_output::bruteforce_vs_importance();
+    //produce_output::bruteforce_vs_importance();
     // produce_output::dim_and_n();
+    produce_output::sgd_noninteracting();
 }
