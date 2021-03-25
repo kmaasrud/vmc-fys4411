29bea390be4007194539bdd69c0d1677cead4911
Anna Stray Rongve <54397743+annasro@users.noreply.github.com>
2021-03-25 17:43:40 +0100

    getting there, need to get the foor loop right

--- vmc/python/analytical_E_vs_a.py
+++ vmc/python/analytical_E_vs_a.py
@@ -7,27 +7,36 @@ PLOT_DIR = "../plots/"
 DATA_DIR = "../data"
 ANA_DIR = "/analytical/dim_and_n/"
 NUM_DIR = "/numerical/dim_and_n/"
-
-
-dim = 1
-particles = 1
-filename_a1 = f"analytical_{dim}D_{particles}_n_part.csv"
-
 dir = DATA_DIR + ANA_DIR
-energy_mean = []
 
 def avglist (list):
     return sum(list) / len(list)
 
-for thread in range(len(os.listdir(dir))):
-    threadID = thread + 2
-    threadID_DIR = f"ThreadId({threadID})/"
-    df = pd.read_csv(dir + threadID_DIR+ filename_a1)
-    energy = df['Energy'].values.tolist()
-    energy_mean.append(energy[0])
-    print(energy_mean)
 
-print(avglist(energy_mean))
+for dim  in range(1,4):
+    for particles in [1]:
+        for alpha in range(0, 90 + 1):
+            alpha *= 0.01
+            for thread in range(len(os.listdir(dir))):
+                threadID = thread + 2
+
+                threadID_DIR = f"ThreadId({threadID})/"
+                filename_a1 = f"analytical_{dim}D_{particles}_n_part.csv"  
+
+                df = pd.read_csv(dir + threadID_DIR + filename_a1)    
+                energy = df['Energy'].values.tolist()
+            
+            
+                energy_list = []
+                
+                energy_list.append(energy[alpha])
+
+                energy_mean = avglist(energy_list)
+                print(avglist(energy_list))
+
+    #df_energy = pd.DataFrame(energy_mean, columns=['Energy'])
+            #print(df_energy)
+        
 
 
 
