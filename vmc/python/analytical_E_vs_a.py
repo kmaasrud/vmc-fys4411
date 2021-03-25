155fbc2838a4520c206cf8cb6727c2188c39e054
Anna Stray Rongve <54397743+annasro@users.noreply.github.com>
2021-03-25 19:36:07 +0100

    avg over energi for one alpha in all threads done, missing  plots and variance (?)

--- vmc/python/analytical_E_vs_a.py
+++ vmc/python/analytical_E_vs_a.py
@@ -13,26 +13,38 @@ def avglist (list):
     return sum(list) / len(list)
 
 
-for dim  in range(1,4):
+for dim in range(1,2):
+    print(f"dim {dim}")
     for particles in [1]:
+        energy_mean = []
         for alpha in range(0, 90 + 1):
-            alpha *= 0.01
+            energy_list = []
+            
             for thread in range(len(os.listdir(dir))):
                 threadID = thread + 2
-
                 threadID_DIR = f"ThreadId({threadID})/"
                 filename_a1 = f"analytical_{dim}D_{particles}_n_part.csv"  
 
                 df = pd.read_csv(dir + threadID_DIR + filename_a1)    
                 energy = df['Energy'].values.tolist()
-            
-            
-                energy_list = []
                 
+            
                 energy_list.append(energy[alpha])
 
-                energy_mean = avglist(energy_list)
-                print(avglist(energy_list))
+            energy_mean.append(avglist(energy_list))
+            a = df['Alpha'].values.tolist()
+        
+        #df_new = pd.DataFrame(list(zip(a, energy_mean)), columns = ['Alpha','Energy'])
+        #pd.to_csv(path)
+        print(df_new)
+        print(len(energy_mean))
+               
+
+           
+            
+        
+                
+        #print(energy_mean)
 
     #df_energy = pd.DataFrame(energy_mean, columns=['Energy'])
             #print(df_energy)
