718cf124925a2778e587a33ac2b8f938dee541dc
kmaasrud <km@aasrud.com>
2021-03-28 16:40:59 +0200

    Jesus this blocking code is bad

--- vmc/python/lib/blocking.py
+++ vmc/python/lib/blocking.py
@@ -17,10 +17,10 @@ def block(x):
 
     # estimate the auto-covariance and variances 
     # for each blocking transformation
-    for i in arange(0,d):
+    for i in range(d):
         n = len(x)
         # estimate autocovariance of x
-        gamma[i] = (n)**(-1)*sum( (x[0:(n-1)]-mu)*(x[1:n]-mu) )
+        gamma[i] = (1/n) * sum((x[0:n-1] - mu)*(x[1:n] - mu))
         # estimate variance of x
         s[i] = var(x)
         # perform blocking transformation
@@ -38,8 +38,16 @@ def block(x):
         # Blocking transformation
         x = 0.5*(x_1 + x_2)
    
-    # generate the test observator M_k from the theorem
-    M = (cumsum( ((gamma/s)**2*2**arange(1,d+1)[::-1])[::-1] )  )[::-1]
+    # Test observator from theorem (chi^2-distributed)
+    factor_1 = (gamma/s)**2
+    factor_2 = 2**arange(1, d+1)
+    # Do the same length check again
+    if (len(factor_1) > len(factor_2)):
+        factor_1 = factor_1[:-1]
+    elif (len(factor_2) > len(factor_1)):
+        factor_2 = factor_2[:-1]
+
+    M = (cumsum((factor_1 * factor_2[::-1])[::-1]))[::-1]
 
     # we need a list of magic numbers
     q = array([6.634897,9.210340, 11.344867, 13.276704, 15.086272, 16.811894, 18.475307, 20.090235, 21.665994, 23.209251, 24.724970, 26.216967, 27.688250, 29.141238, 30.577914, 31.999927, 33.408664, 34.805306, 36.190869, 37.566235, 38.932173, 40.289360, 41.638398, 42.979820, 44.314105, 45.641683, 46.962942, 48.278236, 49.587884, 50.892181])
